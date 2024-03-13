f4429ca7cd15ff3f3f8daed6b4976c618005fb31
PranavPipariya <73152560+PranavPipariya@users.noreply.github.com>
2024-03-13 22:12:09 +0530

    ok

---
+++ src/Words.js
@@ -0,0 +1,24 @@
+import wordBank from "./wordle-bank.txt";
+
+export const boardDefault = [
+  ["", "", "", "", "",""],
+  ["", "", "", "", "",""],
+  ["", "", "", "", "",""],
+  ["", "", "", "", "",""],
+  ["", "", "", "", "",""],
+  ["", "", "", "", "",""],
+];
+
+export const generateWordSet = async () => {
+  let wordSet;
+  let todaysWord;
+  await fetch(wordBank)
+    .then((response) => response.text())
+    .then((result) => {
+      const wordArr = result.split("\n");
+      todaysWord="dhaani";
+      wordSet = new Set(wordArr);
+      console.log(todaysWord);
+    });
+  return { wordSet, todaysWord };
+};
